c5ce8692ec1180ebc8f41090959b14cd9d918091
maxcsu <mnield@csustan.edu>
2026-02-03 17:05:17 -0800

    Add JavaTron Gradle multi-project foundation (client + server)

---
+++ JavaTron/client/core/src/main/java/edu/csu/javatron/JavaTronGame.java
@@ -0,0 +1,11 @@
+package edu.csu.javatron;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class JavaTronGame extends Game {
+    @Override
+    public void create() {
+        setScreen(new FirstScreen());
+    }
+}
---
+++ JavaTron/server/src/main/java/edu/csu/javatron/server/ServerMain.java
@@ -0,0 +1,7 @@
+package edu.csu.javatron.server;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        System.out.println("JavaTron server starting...");
+    }
+}
